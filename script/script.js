9de0c1b78926d05fb6108e8ddeb22ce4c3248f6e
GiovaniBraz02468 <105131834+GiovaniBraz02468@users.noreply.github.com>
2022-06-09 16:43:55 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,15 @@
+function site1(){
+    alert("O site Alexa foi desenvolvido com a função de testar meus conhecimentos com a harmonia das cores, responsividade e o sistema grade-flex.");
+}
+
+function site2(){
+    alert("O site Giofter foi desenvolvido com a função de tesztar meus conhecimentos com o uso do background-image com atributos de posicionamento e fixação.");
+}
+
+function site3(){
+    alert("O site Inter v1.0 (realizado em poucos minutos apenas para 'quebrar o galho'), foi desenvolvido com o propósito de deixar armazenado os aquivos da matéria interdiciplinar da faculdade (Fatec-Catanduva) para meu grupo.");
+}
+
+function info(){
+    alert("Brasileiro, 19 anos, habilitação: A/B\nTrabalhando: Prepara Cursos - NH\nCargo: Assistente pedagógico")
+}
